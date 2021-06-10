2bfcddbefc7e2f07b5616e9dca61b8606606206e
Silas Gonçalves <silasidc@gmail.com>
2021-06-10 18:22:57 -0300

    Modal e forms 1- config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState }from "react";
+import Modal from 'react-modal';
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
 export function App() {
+
+  const [ isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseTransactionModal() {
+      setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={ handleOpenTransactionModal } />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
